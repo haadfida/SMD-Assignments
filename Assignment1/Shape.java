96ca03b9d3b37e95ed999dd035c2ddf8e1fd2cb6
Haad Fida <haad99@gmail.com>
2019-08-31 01:52:44 +0500

    Added abstract and non-abstract methods to Shape

--- Assignment1/Shape.java
+++ Assignment1/Shape.java
@@ -1,4 +1,14 @@
 public abstract class Shape {
     static int numOfObjects;
+    private String name;
     public abstract void draw();
+    public abstract void hasEdges();
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
 }
