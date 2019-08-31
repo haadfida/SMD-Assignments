3b10201223f279ee5cbc56505b73c13603ca8e39
Haad Fida <haad99@gmail.com>
2019-08-31 13:32:10 +0500

    Person along with nested inner class Footballer added

---
+++ Assignment1/Person.java
@@ -0,0 +1,19 @@
+public class Person {
+    static int age;
+    static String name;
+    String nationality;
+
+    public static void showAge(){
+        System.out.println("The age of " + name + " is " + age);
+
+    }
+
+    public static void showName(){
+        System.out.println("The name of the Person is" + name);
+    }
+
+    public static class Teenager{
+        String club;
+
+    }
+}
