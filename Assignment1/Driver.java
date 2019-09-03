bb726f521a5c43d6fed9c8d6d6570c3939d662dd
haadfida <haad99@gmail.com>
2019-09-03 18:22:53 +0500

    Animator methods called in main, Exception Handling added when removing triangle from ShapeList

--- Assignment1/Driver.java
+++ Assignment1/Driver.java
@@ -21,18 +21,26 @@ public class Driver {
             ShapeList.add(triangle);
         }
         catch (Exception e) {
-            System.out.println("An error occured while adding Trinalge to ShapeList");
+            System.out.println("An error occured while adding Trinalge to ShapeList" + "\n");
         }
 
-        System.out.println();
-        System.out.println("Total number of shapes: " + Shape.numOfObjects);
+        System.out.println("Total number of shapes: " + Shape.numOfObjects + "\n");
 
-        System.out.println();
         System.out.println("Deleting Triangle");
         Erase Eraser= new Erase();
-        ShapeList.remove(triangle);
-        System.out.println("Total number of shapes: " + Shape.numOfObjects);
+        try {
+            ShapeList.remove(triangle);
+        }
+        catch (Exception e) {
+            System.out.println("An error occured while removing Trinalge from ShapeList" + "\n");
+        }
 
+        System.out.println("Total number of shapes: " + Shape.numOfObjects + "\n");
+
+        Animator animate = new Animator();
+        animate.Animator();
+
+        System.out.println();
         Player player=new Player(19,"Haad","Pakistan");
         Player.Teenager teen = new Player.Teenager(player, "Lahore Kalandar");
 
