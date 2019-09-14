728934669682c45022371ea4068a34b7df5d64a5
haadfida <haad99@gmail.com>
2019-09-14 23:03:45 +0500

    Added linear and frame layouts

--- Assignment2-GUI/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Assignment2-GUI/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -32,4 +32,12 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, TabbedActivity.class);
         startActivity(intent);
     }
+
+    public void clickexit(View v)
+    {
+        moveTaskToBack(true);
+        android.os.Process.killProcess(android.os.Process.myPid());
+        System.exit(1);
+
+    }
 }
